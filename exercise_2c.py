5921b3e354dde6c322e9a2afee94b5831ae19c42
deboahemaa <78213392+deboahemaa@users.noreply.github.com>
2022-06-09 16:06:17 +0200

    Add files via upload

---
+++ exercise_2c.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def euler(h):
+    y = 1
+    t = 0
+    y_list =[y]
+    t_list = [t]
+    y_anal_list = [1]
+    dev = 0 
+    max_dev = 0
+
+    for time in np.arange(h, 2, h):
+        y = y + h*(time - 2*y)
+        y_anal = 0.25 * (2*time - 1 + 5 * np.exp(-2*time))
+        y_list.append(y)
+        t_list.append(time)
+        y_anal_list.append(np.abs(y - y_anal))
+        dev += np.abs(y - y_anal)
+    
+        if np.abs(y - y_anal) > max_dev:
+            max_dev = np.abs(y - y_anal)
+    
+    global_error = dev / len(t_list)
+    return t_list, y_list, y_anal_list, max_dev, global_error
+
+def plotjes():
+    f1 = euler(0.2)
+    f2 = euler(0.1)
+    f3 = euler(0.05)
+    f4 = euler(0.025)
+    plt.plot([0.2, 0.1, 0.05, 0.025], [f1[3], f2[3], f3[3], f4[3]])
+    fig, ax = plt.subplots(4)
+    ax[0].plot(f1[0], f1[1])
+    ax[1].plot(f2[0], f2[1])
+    ax[2].plot(f3[0], f3[1]) 
+    ax[3].plot(f4[0], f4[1]) 
+    
+    plt.show()
+    return
+
+plotjes()
